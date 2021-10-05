da19aba67009d9116cc6ed8d26168e6296c4ff4c
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-10-06 01:48:21 +0300

    Add task 5

--- homework_04_regexp/source/js/index.js
+++ homework_04_regexp/source/js/index.js
@@ -2,6 +2,7 @@ const button_1 = document.getElementById("1");
 const button_2 = document.getElementById("2");
 const button_3 = document.getElementById("3");
 const button_4 = document.getElementById("4");
+const button_5 = document.getElementById("5");
 
 const task_1 = () => {
   const input = prompt("Введите две строки через запятую");
@@ -39,9 +40,9 @@ const task_2 = () => {
 
 const task_3 = () => {
   const input = prompt("Введите строку с датой и временем в формате \"12/02/2021 12-00\"");
-  const testDate = /^\d{2}\/\d{2}\/\d{4} \d{2}-\d{2}$/;
+  const dateTest = /^\d{2}\/\d{2}\/\d{4} \d{2}-\d{2}$/;
 
-  if(input && input.match(testDate)) {
+  if(input && input.match(dateTest)) {
     const firstChange = /\//g;
     const secondChange = /-/g;
     alert(input.replace(/\//g, ".").replace(/-/g, ":"));
@@ -63,7 +64,20 @@ const task_4 = () => {
   }
 }
 
+const task_5 = () => {
+  const input = prompt("Введите строку кириллических символов (проверка ФИО)");
+  const replace = (value) => `_${value.toLowerCase()}`;
+  const camelCaseTest = /^([A-ZА-Я]{1}[a-zа-я]+){2,}$/;
+
+  if(input && camelCaseTest.test(input)) {
+    alert(input.replace(/[A-Z]/g, replace).slice(1))
+  } else if(input !== null) {
+    alert("Необходимо выполнить условие задачи!");
+  }
+}
+
 button_1.addEventListener("click", task_1);
 button_2.addEventListener("click", task_2);
 button_3.addEventListener("click", task_3);
 button_4.addEventListener("click", task_4);
+button_5.addEventListener("click", task_5);
