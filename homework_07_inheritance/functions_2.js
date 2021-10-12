d8fde5b1bbb487d0fd1c6f4a86d3e3c181576a8c
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-10-13 01:40:39 +0300

    Add second function realization

---
+++ homework_07_inheritance/functions_2.js
@@ -0,0 +1,95 @@
+function Animal() {
+  const name = "Неизвестное животное";
+
+  return function() {
+
+    Object.defineProperties(this, {
+      say: {
+        value: function() {
+          console.log("Неизвестное животное молчит...");
+        },
+        writable: true,
+        enumerable: true,
+        configurable: true
+      },
+      eat: {
+        value: function() {
+          console.log(`${this.name || name} ест...`);
+        },
+        writable: false,
+        enumerable: false,
+        configurable: false
+      },
+      rename: {
+        value: function(name) {
+          const regex = /^[А-Яа-я- ]+$/;
+
+          if(regex.test(name) && name.trim().length) {
+            Object.defineProperty(this, "name", {
+              value: name
+            });
+          }
+        },
+        writable: false,
+        enumerable: false,
+        configurable: false
+      },
+    });
+  }
+};
+
+function Cat() {
+  const parent = new Animal();
+  parent.call(this);
+
+  Object.defineProperties(this, {
+    say: {
+      value: function() {
+        console.log("Кот говорит мяу...");
+      },
+      writable: false,
+      enumerable: false,
+      configurable: false
+    },
+    hunt: {
+      value: function() {
+        console.log(`${this.name} охотится...`);
+      },
+      writable: false,
+      enumerable: false,
+      configurable: false
+    }
+  });
+}
+
+function Dog() {
+  const parent = new Animal();
+  parent.call(this);
+
+  Object.defineProperties(this, {
+    say: {
+      value: function() {
+        console.log("Собака говорит гав...");
+      },
+      writable: false,
+      enumerable: false,
+      configurable: false
+    }
+  });
+}
+
+function Parrot() {
+  const parent = new Animal();
+  parent.call(this);
+
+  Object.defineProperties(this, {
+    say: {
+      value: function() {
+        console.log("Попугай говорит: 'Кто тут мяукает и гавкает?'...");
+      },
+      writable: false,
+      enumerable: false,
+      configurable: false
+    }
+  });
+}
