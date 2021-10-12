9e08f71fca351f2e8fb44ac95e69c4453dedc84e
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-10-13 01:53:09 +0300

    Add classes realization

---
+++ homework_07_inheritance/classes.js
@@ -0,0 +1,47 @@
+class Animal {
+  constructor() {
+    this._name = "Неизвестное животное";
+  }
+
+  eat() {
+    console.log(`${this._name} ест...`);
+  };
+
+  say() {
+    console.log("Неизвестное животное молчит...");
+  };
+
+  set name(name) {
+    const regex = /^[А-Яа-я- ]+$/;
+
+    if(regex.test(name) && name.trim().length) {
+      this._name = name;
+    }
+  };
+
+  get name() {
+    return this._name;
+  }
+}
+
+class Cat extends Animal {
+  say() {
+    console.log("Кот говорит мяу...");
+  };
+
+  hunt() {
+    console.log(`${this._name} охотится...`);
+  };
+}
+
+class Dog extends Animal {
+  say() {
+    console.log("Собака говорит гав...");
+  };
+}
+
+class Parrot extends Animal {
+  say() {
+    console.log("Попугай говорит: 'Кто тут мяукает и гавкает?'...");
+  };
+}
