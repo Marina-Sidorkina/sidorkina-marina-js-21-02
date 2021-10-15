136f6392b3b23dfd7ea97250465d389813f7cdca
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-10-15 23:41:10 +0300

    Add task 1

---
+++ homework_09_async/task_1/setInterval.js
@@ -0,0 +1,27 @@
+// Реализация через setInterval
+
+const result = document.querySelector(".result__window");
+const form = document.querySelector(".form");
+const startElement = document.querySelector("#start");
+const endElement = document.querySelector("#end");
+let id;
+
+let intervalCount = (start, end) => {
+  id = setInterval(function() {
+    if(start <= end) {
+      result.innerText = start;
+      start++;
+    } else {
+      clearInterval(id)
+    }
+  }, 1000);
+}
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  clearInterval(id);
+  result.innerText = "";
+  let start = parseInt(startElement.value, 10);
+  let end = parseInt(endElement.value, 10);
+  intervalCount(start, end);
+})
---
+++ homework_09_async/task_1/setTimeout.js
@@ -0,0 +1,28 @@
+// Реализация через setTimeout
+
+const result = document.querySelector(".result__window");
+const form = document.querySelector(".form");
+const startElement = document.querySelector("#start");
+const endElement = document.querySelector("#end");
+let id;
+
+let timeoutCount = (start, end) => {
+  id = setTimeout(function changeValue(start, end) {
+    if(start <= end) {
+      result.innerText = start;
+      start++
+      id = setTimeout(changeValue, 1000, start, end);
+    } else {
+      clearTimeout(id)
+    }
+  }, 1000, start, end);
+}
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  clearTimeout(id);
+  result.innerText = "";
+  let start = parseInt(startElement.value, 10);
+  let end = parseInt(endElement.value, 10);
+  timeoutCount(start, end);
+})
