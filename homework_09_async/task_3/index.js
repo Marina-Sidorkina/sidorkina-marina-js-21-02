ae7d016c78d7b0c3aa4429c8398f546cfb5ba216
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-10-16 04:16:43 +0300

    Add sorting

--- homework_09_async/task_3/index.js
+++ homework_09_async/task_3/index.js
@@ -1,8 +1,65 @@
 const listBodyElement = document.querySelector(".list__body");
 const controlElement = document.querySelector(".controls");
+const sortButtonHeightElement = document.querySelector(".sort__button_height");
+const sortButtonMassElement = document.querySelector(".sort__button_mass");
+const sortButtonNameElement = document.querySelector(".sort__button_name");
+const sortButtonGenderElement = document.querySelector(".sort__button_gender");
 const URL_MAIN = "https://swapi.dev/api/people/";
+const HEIGHT_INDEX = 3;
+const MASS_INDEX = 2;
 let next;
 let prev;
+let currentList = [];
+let heightSorting = "up";
+let massSorting = "up";
+
+const isNumeric = (value) => {
+  return /^\d+$/.test(value);
+}
+
+const sortByNumberValues = (index, sortingCategory) => {
+  const numbers = currentList.filter((value) => isNumeric(value[index].replace(",","")));
+  const unknown = currentList.filter((value) => !isNumeric(value[index].replace(",","")));
+  if(sortingCategory === "up") {
+    numbers.sort((a, b) => parseInt(a[index].replace(",",""), 10) - parseInt(b[index].replace(",",""), 10));
+  } else {
+    numbers.sort((a, b) => parseInt(b[index].replace(",",""), 10) - parseInt(a[index].replace(",",""), 10));
+  }
+
+  currentList = [...numbers, ...unknown]
+  listBodyElement.innerHTML = "";
+  currentList.forEach((item) => createNewListItem(item[0], item[1], item[2], item[3]));
+}
+
+const sortByHeight = () => {
+  sortByNumberValues(HEIGHT_INDEX, heightSorting);
+  heightSorting = heightSorting === "up" ? "down" : "up";
+}
+
+const sortByMass = () => {
+  sortByNumberValues(MASS_INDEX, massSorting);
+  massSorting = massSorting === "up" ? "down" : "up";
+}
+
+const sortByAlphabet = (first, second) => first[0].localeCompare(second[0]);
+
+const sortByName = () => {
+  currentList.sort(sortByAlphabet);
+  listBodyElement.innerHTML = "";
+  currentList.forEach((item) => createNewListItem(item[0], item[1], item[2], item[3]));
+}
+
+const sortByGender = () => {
+  const male = currentList.filter((item) => item[1] === "male");
+  const female = currentList.filter((item) => item[1] === "female");
+  const unknown  = currentList.filter((item) => item[1] === "n/a");
+  const hermaphrodite = currentList.filter((item) => item[1] === "hermaphrodite");
+  const none = currentList.filter((item) => item[1] === "none");
+
+  currentList = [...male, ...female, ...unknown, ...hermaphrodite, ...none];
+  listBodyElement.innerHTML = "";
+  currentList.forEach((item) => createNewListItem(item[0], item[1], item[2], item[3]));
+}
 
 const createNewListItem = (name, gender, mass, height) => {
   const documentFragment = document.createDocumentFragment();
@@ -42,8 +99,10 @@ const changePage = (evt) => {
 }
 
 const serveResponse = (response) => {
+  currentList = [];
   response.results.forEach((item) => {
-    createNewListItem(item["name"], item["gender"], item["mass"], item["height"])
+    createNewListItem(item["name"], item["gender"], item["mass"], item["height"]);
+    currentList.push([item["name"], item["gender"], item["mass"], item["height"]]);
   });
   next = response["next"];
   prev = response["previous"];
@@ -57,4 +116,8 @@ const swapi = (url) => {
 
 swapi(URL_MAIN);
 
-controlElement.addEventListener("click", changePage)
+controlElement.addEventListener("click", changePage);
+sortButtonHeightElement.addEventListener("click", sortByHeight);
+sortButtonMassElement.addEventListener("click", sortByMass);
+sortButtonNameElement.addEventListener("click", sortByName);
+sortButtonGenderElement.addEventListener("click", sortByGender);
