5480ef88ade6bd6725717492e79f433a295c6256
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-10-19 22:15:20 +0300

    Add fixes

--- homework_10_localStorage/js/api/imgbb.js
+++ homework_10_localStorage/js/api/imgbb.js
@@ -2,8 +2,9 @@ import { createFetch } from "../utils/create-fetch.js";
 import { API_URL } from "../constants/imgbb.js";
 import {updateImgArr, updateLocalStorage} from "../services/local-storage.js";
 import {addImg} from "../components/gallery-list.js";
-import {clearFormInfo} from "../components/form-info.js";
+import { clearFormInfo } from "../components/form-info.js";
 import { stopLoader } from "../components/loader.js";
+import { hideErrorElement, showErrorElement} from "../components/error.js";
 
 export const imgbb = {
   getImageUrl: createFetch(API_URL),
@@ -12,10 +13,12 @@ export const imgbb = {
     updateLocalStorage();
     addImg(response.data.display_url);
     clearFormInfo();
+    hideErrorElement();
     stopLoader();
   },
-  onError: (error) => {
-    console.log(error);
+  onError: () => {
+    showErrorElement();
+    clearFormInfo();
     stopLoader();
   }
 }
