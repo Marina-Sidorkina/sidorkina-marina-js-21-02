b91126b72af08bce04f87e843e9a468a2e9244d8
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-10-25 22:14:42 +0300

    Create app

---
+++ homework_12_react/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <header className="header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+    </React.Fragment>
+  );
+}
+
+export default App;
