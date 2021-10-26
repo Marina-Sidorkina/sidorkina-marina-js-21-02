ae71439db33e037ece2420a233105862fef2c5dd
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-10-26 14:08:11 +0300

    Add filter mock data

---
+++ homework_12_react/src/mocks/filter.tsx
@@ -0,0 +1,32 @@
+export const filter = {
+  grandPianos: [
+    {
+      name: "Yamaha",
+      id: "gp-1"
+    },
+    {
+      name: "Schimmel",
+      id: "gp-2"
+    },
+    {
+      name: "Kawai",
+      id: "gp-3"
+    },
+    {
+      name: "Fazioli",
+      id: "gp-4"
+    }],
+  pianos: [
+    {
+      name: "Yamaha",
+      id: "p-1"
+    },
+    {
+      name: "Schimmel",
+      id: "p-2"
+    },
+    {
+      name: "Kawai",
+      id: "p-3"
+    }]
+}
