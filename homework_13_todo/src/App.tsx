26e85e36f313e3ed5dd318c6bf805d1e80955709
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-10-28 13:29:57 +0300

    Add header

---
+++ homework_13_todo/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header app__header">
+        <h1 className="header__title">TodoList</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
