55b5b9a3803f46334599ea8eadd920f9d8742312
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-10-30 16:48:15 +0300

    Add helper

--- homework_14_context/src/components/usersList/UsersList.tsx
+++ homework_14_context/src/components/usersList/UsersList.tsx
@@ -2,21 +2,25 @@ import React from "react";
 import "./UsersList.css";
 import { IUsersListProps } from "../../@types/interfaces/components";
 import User from "../user/User";
+import helper from "../../hocs/helper/helper";
 
 class UsersList extends React.Component<IUsersListProps> {
   render() {
     const { list } = this.props;
 
-    const elements = list.map((item, index) => (
-      <li className="users-list__item" key={ item.id }>
-        <User id={ item.id }
-              title={ item.title }
-              firstName={ item.firstName }
-              lastName={ item.lastName }
-              picture={ item.picture }
-              index={ index }/>
-      </li>
-    ));
+    const elements = list.map((item, index) => {
+      const UserWithHelper = helper(User, item.id);
+      return (
+        <li className="users-list__item" key={item.id}>
+          <UserWithHelper id={item.id}
+                title={item.title}
+                firstName={item.firstName}
+                lastName={item.lastName}
+                picture={item.picture}
+                index={index}/>
+        </li>
+      )
+    });
 
     return (
       <ul className="users-list main__users-list">
