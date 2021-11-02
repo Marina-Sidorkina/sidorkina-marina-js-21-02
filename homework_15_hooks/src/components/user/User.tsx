d458315296c3f45fa1b06006e3f38fecd3b539be
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-11-02 17:58:53 +0300

    Refactor User

--- homework_15_hooks/src/components/user/User.tsx
+++ homework_15_hooks/src/components/user/User.tsx
@@ -2,22 +2,20 @@ import React from "react";
 import "./User.scss";
 import { IUserProps } from "../../@types/interfaces/components";
 
-class User extends React.Component<IUserProps> {
-  render() {
-    const { id, title, firstName, lastName, picture, index, className } = this.props;
+const User = (props: IUserProps) => {
+  const { id, title, firstName, lastName, picture, index, className } = props;
 
-    return (
-      <div className={ `user ${ className }` }>
-        <img className={`user__img ${ index === 0 ? "user__img_first" : ""}`} src={ picture } alt="User"/>
-        <div className="user__info">
-          <div className="user__id">{ id }</div>
-          <span className="user__title">{ `${title}.`} </span>
-          <span className="user__name">{ firstName } </span>
-          <span className="user__surname">{ lastName }</span>
-        </div>
+  return (
+    <div className={ `user ${ className }` }>
+      <img className={`user__img ${ index === 0 ? "user__img_first" : ""}`} src={ picture } alt="User"/>
+      <div className="user__info">
+        <div className="user__id">{ id }</div>
+        <span className="user__title">{ `${title}.`} </span>
+        <span className="user__name">{ firstName } </span>
+        <span className="user__surname">{ lastName }</span>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default User;
