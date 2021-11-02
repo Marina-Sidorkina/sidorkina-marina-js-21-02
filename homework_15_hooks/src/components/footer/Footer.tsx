6a1e40fdd69fb5def8f47f1057d48f105482f5ec
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-11-02 15:52:58 +0300

    Refactor Header, Footer, Limit

--- homework_15_hooks/src/components/footer/Footer.tsx
+++ homework_15_hooks/src/components/footer/Footer.tsx
@@ -4,17 +4,15 @@ import Theme from "../theme/Theme";
 import Paginator from "../paginator/Paginator";
 import {IFooterProps} from "../../@types/interfaces/components";
 
-class Footer extends  React.Component<IFooterProps> {
-  render() {
-    return (
-      <footer className="footer">
-        <Paginator onCurrentChange={ this.props.onPageChange }
-                   current={ this.props.currentPage }
-                   className={ this.props.paginatorClassName }/>
-        <Theme className={ this.props.themeClassName }/>
-      </footer>
-    );
-  }
+const Footer = (props: IFooterProps) => {
+  return (
+    <footer className="footer">
+      <Paginator onCurrentChange={ props.onPageChange }
+                 current={ props.currentPage }
+                 className={ props.paginatorClassName }/>
+      <Theme className={ props.themeClassName }/>
+    </footer>
+  );
 }
 
 export default Footer;
--- homework_15_hooks/src/components/header/Header.tsx
+++ homework_15_hooks/src/components/header/Header.tsx
@@ -4,16 +4,14 @@ import { IHeaderProps } from "../../@types/interfaces/components";
 import Limit from "../limit/Limit";
 import Loader from "../loader/Loader";
 
-class Header extends React.Component<IHeaderProps> {
-  render() {
-    return (
-      <header className={`header app__header ${ this.props.className }`}>
-        <h1 className="header__title">Пользователи</h1>
-        { this.props.isLoading ? <Loader/> : null }
-        <Limit onValueChange={ this.props.onLimitChange }/>
-      </header>
-    );
-  }
+const Header = (props: IHeaderProps) => {
+  return (
+    <header className={`header app__header ${ props.className }`}>
+      <h1 className="header__title">Пользователи</h1>
+      { props.isLoading ? <Loader/> : null }
+      <Limit onValueChange={ props.onLimitChange }/>
+    </header>
+  );
 }
 
 export default Header;
