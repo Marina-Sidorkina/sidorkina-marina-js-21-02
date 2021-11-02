adcb9e557081f92b82650f6d28e740153fbf9fd3
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-11-02 17:45:04 +0300

    Refactor Paginator

--- homework_14_context/src/components/paginator/Paginator.tsx
+++ homework_14_context/src/components/paginator/Paginator.tsx
@@ -1,26 +1,34 @@
 import React, {SyntheticEvent} from "react";
-import "./Paginator.css";
+import "./Paginator.scss";
 import { IPaginatorProps } from "../../@types/interfaces/components";
 
-const Paginator = (props: IPaginatorProps) => {
-  const onCurrentChange = (evt: SyntheticEvent) => {
-    const value = parseInt((evt.target as HTMLElement).innerText, 10);
-    props.onCurrentChange(value);
+class Paginator extends React.Component<IPaginatorProps> {
+  constructor(props: IPaginatorProps) {
+    super(props);
+
+    this.onCurrentChange = this.onCurrentChange.bind(this);
   }
 
-  const elements = [...Array(5)].map((_, index) => (
-    <li className="paginator__item" key={ index } onClick={ onCurrentChange }>
-      <a className={`paginator__link ${ props.current === index + 1 ?
-        "paginator__link_current" : ""}`}
-         href="#">{ index + 1 }</a>
-    </li>
-  ));
+  onCurrentChange(evt: SyntheticEvent) {
+    const value = parseInt((evt.target as HTMLElement).innerText, 10);
+    this.props.onCurrentChange(value);
+  }
 
-  return (
-    <ul className={ `paginator ${ props.className }` }>
-      { elements }
-    </ul>
-  );
+  render() {
+    const elements = [...Array(5)].map((_, index) => (
+      <li className="paginator__item" key={ index } onClick={ this.onCurrentChange }>
+        <a className={`paginator__link ${ this.props.current === index + 1 ?
+          "paginator__link_current" : ""}`}
+           href="#">{ index + 1 }</a>
+      </li>
+    ));
+
+    return (
+      <ul className={ `paginator ${ this.props.className }` }>
+        { elements }
+      </ul>
+    );
+  }
 }
 
 export default Paginator;
