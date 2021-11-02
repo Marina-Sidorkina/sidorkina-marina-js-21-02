a23c51938c107772214fb1805ed19860c157476e
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-11-02 18:00:28 +0300

    Refactor UsersList

--- homework_15_hooks/src/components/usersList/UsersList.tsx
+++ homework_15_hooks/src/components/usersList/UsersList.tsx
@@ -4,31 +4,30 @@ import { IUsersListProps } from "../../@types/interfaces/components";
 import User from "../user/User";
 import helper from "../../hocs/helper/helper";
 
-class UsersList extends React.Component<IUsersListProps> {
-  render() {
-    const { list, userClassName } = this.props;
+const UsersList = (props: IUsersListProps) => {
+  const { list, userClassName } = props;
 
-    const elements = list.map((item, index) => {
-      const UserWithHelper = helper(User, item.id);
-      return (
-        <li className="users-list__item" key={item.id}>
-          <UserWithHelper id={item.id}
-                title={item.title}
-                firstName={item.firstName}
-                lastName={item.lastName}
-                picture={item.picture}
-                index={index}
-                className={ userClassName }/>
-        </li>
-      )
-    });
+  const elements = list.map((item, index) => {
+    const UserWithHelper = helper(User, item.id);
 
     return (
-      <ul className="users-list main__users-list">
-        { elements }
-      </ul>
-    );
-  }
+      <li className="users-list__item" key={item.id}>
+        <UserWithHelper id={item.id}
+                        title={item.title}
+                        firstName={item.firstName}
+                        lastName={item.lastName}
+                        picture={item.picture}
+                        index={index}
+                        className={ userClassName }/>
+      </li>
+    )
+  });
+
+  return (
+    <ul className="users-list main__users-list">
+      { elements }
+    </ul>
+  );
 }
 
 export default UsersList;
