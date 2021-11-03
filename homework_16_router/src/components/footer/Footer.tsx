1a63558c4013c5ae693ec433cb330bd0c7561a06
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-11-04 02:42:53 +0300

    Copy project

---
+++ homework_16_router/src/components/footer/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.scss";
+import Theme from "../theme/Theme";
+import Paginator from "../paginator/Paginator";
+import { IFooterProps } from "../../@types/interfaces/components";
+
+const Footer = (props: IFooterProps) => {
+  return (
+    <footer className="footer">
+      <Paginator onCurrentChange={ props.onPageChange }
+                 current={ props.currentPage }/>
+      <Theme />
+    </footer>
+  );
+}
+
+export default Footer;
