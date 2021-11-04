25278bc59019176c75836e0bac391af0e5456e7c
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-11-05 01:48:22 +0300

    Refactor api

--- homework_16_router/src/constants/dummyApi.tsx
+++ homework_16_router/src/constants/dummyApi.tsx
@@ -2,4 +2,6 @@ export const BASE_URL = "https://dummyapi.io/data/v1/";
 export const METHOD_GET = "GET";
 export const API_ID = "617d32e4477c9955936d6413";
 export const API_ID_FIELD = "app-id";
-
+export const USER_URL = "user";
+export const PAGE_FIELD = "page";
+export const LIMIT_FIELD = "limit";
