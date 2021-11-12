9137bae38e8c3a7148b76c48d9cf7be129f7d9bc
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-11-12 05:32:13 +0300

    Change api id

--- homework_15_hooks/src/constants/dummyApi.tsx
+++ homework_15_hooks/src/constants/dummyApi.tsx
@@ -1,5 +1,5 @@
 export const BASE_URL = "https://dummyapi.io/data/v1/";
 export const METHOD_GET = "GET";
-export const API_ID = "618b946c0c847f9cda16c4f7";
+export const API_ID = "618dd1b5e3143ec8c984062d";
 export const API_ID_FIELD = "app-id";
 
