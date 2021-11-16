9ee4f9f17b70a92740ea8a12e5037ffebb7e8e62
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-11-17 00:43:15 +0300

    Add fixes

--- homework_19_redux/src/components/loader/Loader.tsx
+++ homework_19_redux/src/components/loader/Loader.tsx
@@ -1,12 +1,11 @@
 import "./Loader.scss";
 import { connect } from "react-redux";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { changeLoaderWidthAction } from "../../redux/actions/loader";
 import { bindActionCreators } from "redux";
 
 const Loader = (props: any) =>  {
   const { width, changeLoaderWidth } = props;
-  const isUnmounted = useRef(false);
 
   useEffect(() => {
     changeLoaderWidth(0);
@@ -14,20 +13,16 @@ const Loader = (props: any) =>  {
 
   useEffect(() => {
     let id = 0;
-    isUnmounted.current = false;
 
-    if (!isUnmounted.current) {
-      const showLoadingProcess = () => {
-        changeLoaderWidth(width < 300 ? width + 1 : 0);
-        id = requestAnimationFrame(showLoadingProcess);
-      }
-
+    const showLoadingProcess = () => {
+      changeLoaderWidth(width < 300 ? width + 1 : 0);
       id = requestAnimationFrame(showLoadingProcess);
     }
 
+    id = requestAnimationFrame(showLoadingProcess);
+
     return () => {
       cancelAnimationFrame(id);
-      isUnmounted.current = true;
     };
   }, [changeLoaderWidth, width])
 
