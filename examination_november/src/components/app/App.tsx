aadb56b7b90eaaefd29019788a71ead680eeb03b
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-11-22 17:02:03 +0300

    Fix userInfo

--- examination_november/src/components/app/App.tsx
+++ examination_november/src/components/app/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.scss';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
-import Users from '../../pages/users/Users';
+import Profile from '../../pages/profile/Profile';
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <Users />
+      <Profile />
       <Footer />
     </div>
   );
