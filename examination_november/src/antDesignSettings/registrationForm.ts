f970ca51a0cce996ac858ab910efdd6e6960ce87
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-11-27 14:41:58 +0300

    Refactor registration form

---
+++ examination_november/src/antDesignSettings/registrationForm.ts
@@ -0,0 +1,50 @@
+export const RULES = {
+  name: [
+    {
+      validator(rule: any, value: string) {
+        let check;
+        if (value) {
+          const values = value.split(' ');
+          check = values[1] ? values[0].length >= 2 && values[1].length >= 2
+            && values[0].length <= 50 && values[1].length <= 50 : false;
+        }
+        if (!check) return Promise.reject(new Error('Минимум по 2, максимум по 50 зн.'));
+
+        return Promise.resolve();
+      }
+    }
+  ],
+  gender: [
+    { required: true, message: 'Укажите пол' }
+  ],
+  date: [
+    { required: true, message: 'Укажите дату рождения' },
+    {
+      pattern: /^(0[1-9]|1[012])[.](0[1-9]|[12][0-9]|3[01])[.](19|20)\d\d$/,
+      message: 'Формат: ММ.ДД.ГГГГ'
+    },
+    {
+      validator(rule: any, value: any) {
+        const today = new Date();
+        const dateArray = value ? value.split('.').map((item: string) => parseInt(item, 10)) : null;
+        const check = value ? new Date(dateArray[2], dateArray[0] - 1, dateArray[1]) : null;
+
+        if (check && check > today) return Promise.reject(new Error('Invalid date'));
+
+        return Promise.resolve();
+      }
+    }
+  ],
+  email: [
+    { required: true, message: ' Укажите email' },
+    {
+      pattern: /^[a-z0-9]+[a-z-_0-9]+[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/,
+      message: 'Буквы и числа@буквы.буквы'
+    }
+  ],
+  tel: [
+    { required: true, message: 'Укажите телефон' },
+    { min: 5, message: 'Минимум 5' },
+    { max: 20, message: 'Максимум 20' }
+  ]
+};
--- examination_november/src/components/formComponents/registrationForm/RegistrationForm.tsx
+++ examination_november/src/components/formComponents/registrationForm/RegistrationForm.tsx
@@ -20,6 +20,7 @@ import {
   updateNameAction, updatePhoneAction
 } from '../../../redux/actions/registrationForm';
 import { ThemeContext } from '../../../contexts/ThemeContext';
+import { RULES } from '../../../antDesignSettings/registrationForm';
 
 interface IRegistrationFormProps {
   authorize: Function;
@@ -87,21 +88,7 @@ const RegistrationForm = (props: IRegistrationFormProps) => {
           <Form.Item
             className="registration__form-item registration__form-item_name"
             name="registration__name"
-            rules={[
-              {
-                validator(rule: any, value: string) {
-                  let check;
-                  if (value) {
-                    const values = value.split(' ');
-                    check = values[1] ? values[0].length >= 2 && values[1].length >= 2
-                      && values[0].length <= 50 && values[1].length <= 50 : false;
-                  }
-                  if (!check) return Promise.reject(new Error('Минимум по 2, максимум по 50 зн.'));
-
-                  return Promise.resolve();
-                }
-              }
-            ]}
+            rules={RULES.name}
           >
             <Input
               className="registration__input"
@@ -116,9 +103,7 @@ const RegistrationForm = (props: IRegistrationFormProps) => {
           <Form.Item
             className="registration__form-item registration__form-item_radio"
             name="registration__gender"
-            rules={[
-              { required: true, message: 'Укажите пол' }
-            ]}
+            rules={RULES.gender}
           >
             <Radio.Group
               value={gender}
@@ -135,24 +120,7 @@ const RegistrationForm = (props: IRegistrationFormProps) => {
           <Form.Item
             className="registration__form-item"
             name="registration__date"
-            rules={[
-              { required: true, message: 'Укажите дату рождения' },
-              {
-                pattern: /^(0[1-9]|1[012])[.](0[1-9]|[12][0-9]|3[01])[.](19|20)\d\d$/,
-                message: 'Формат: ММ.ДД.ГГГГ'
-              },
-              {
-                validator(rule: any, value: any) {
-                  const today = new Date();
-                  const dateArray = value ? value.split('.').map((item: string) => parseInt(item, 10)) : null;
-                  const check = value ? new Date(dateArray[2], dateArray[0] - 1, dateArray[1]) : null;
-
-                  if (check && check > today) return Promise.reject(new Error('Invalid date'));
-
-                  return Promise.resolve();
-                }
-              }
-            ]}
+            rules={RULES.date}
           >
             <Input
               className="registration__input"
@@ -167,13 +135,7 @@ const RegistrationForm = (props: IRegistrationFormProps) => {
           <Form.Item
             className="registration__form-item"
             name="registration__email"
-            rules={[
-              { required: true, message: ' Укажите email' },
-              {
-                pattern: /^[a-z0-9]+[a-z-_0-9]+[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/,
-                message: 'Буквы и числа@буквы.буквы'
-              }
-            ]}
+            rules={RULES.email}
           >
             <Input
               className="registration__input"
@@ -188,11 +150,7 @@ const RegistrationForm = (props: IRegistrationFormProps) => {
           <Form.Item
             className="registration__form-item"
             name="registration__tel"
-            rules={[
-              { required: true, message: 'Укажите телефон' },
-              { min: 5, message: 'Минимум 5' },
-              { max: 20, message: 'Максимум 20' }
-            ]}
+            rules={RULES.tel}
           >
             <Input
               className="registration__input"
