764f1e59c6c7aaf021e498a38a71565a1dc582c2
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-12-10 19:47:39 +0300

    Add fixes

--- examination_december_1/server/utils/parseDate.js
+++ examination_december_1/server/utils/parseDate.js
@@ -1,11 +1,29 @@
+const MONTHS_RU = ['Января', 'Февраля', 'Марта',
+  'Апреля', 'Мая', 'Июня', 'Июля', 'Августа',
+  'Сентября', 'Октября', 'Ноября', 'Декабря'
+];
+
+const MONTHS_EN = ['of January', 'of February', 'of March',
+  'of April', 'of May', 'of June', 'of July', 'of August',
+  'of September', 'of October', 'of November', 'of December'
+];
+
+const CONTROL_VALUE = 10;
+const PREFIX = '0';
+const YEAR = 'года';
+
+const getValue = (value) => {
+  if (value < CONTROL_VALUE) return `${PREFIX}${value}`;
+  return value;
+};
+
 const parseDate = (isoDate) => {
   const date = new Date(isoDate);
+  const time = `${getValue(date.getHours())}:${getValue(date.getMinutes())}`;
   return {
-    day: date.getDate(),
-    month: date.getMonth(),
-    year: date.getFullYear(),
-    hours: date.getHours(),
-    minutes: date.getMinutes()
+    en: `${date.getDate()} ${MONTHS_EN[date.getMonth()]} ${date.getFullYear()}`,
+    ru: `${date.getDate()} ${MONTHS_RU[date.getMonth()]} ${date.getFullYear()} ${YEAR}`,
+    time: time
   }
 }
 
