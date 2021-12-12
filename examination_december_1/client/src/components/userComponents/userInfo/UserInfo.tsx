279c52b9a5bef08885c69fd7eb994a0542d99270
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-12-13 02:57:33 +0300

    Refactor userInfo and userInfoIcon styles

--- examination_december_1/client/src/components/userComponents/userInfo/UserInfo.tsx
+++ examination_december_1/client/src/components/userComponents/userInfo/UserInfo.tsx
@@ -1,9 +1,9 @@
 import React, { useContext, useEffect } from 'react';
-import './UserInfo.scss';
 import { useDispatch } from 'react-redux';
 import { Spin } from 'antd';
 import { useParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import styles from './UserInfo.module.scss';
 import { loadUserInfo } from '../../../redux/actions/userInfo';
 import { getGenderFieldValue, getTitleValue } from '../../../utils/components';
 import { DEFAULT_IMAGE, RUSSIAN_LANGUAGE } from '../../../constants/components';
@@ -38,7 +38,6 @@ const UserInfo = () => {
   const element = stateValues.userInfo.data.isLoading
     ? (
       <Spin
-        className="users-info__spinner"
         tip={t('loadingText', {})}
         size="small"
         style={{
@@ -52,25 +51,25 @@ const UserInfo = () => {
     ) : (
       <>
         <img
-          className="user-info__image"
+          className={styles.image}
           src={stateValues.userInfo.data.user.picture || DEFAULT_IMAGE}
           alt="Аватар пользователя"
         />
-        <div className="user-info__container">
-          <div className="user-info__details">
-            <p className="user-info__name">
+        <div className={styles.container}>
+          <div className={styles.details}>
+            <p className={styles.name}>
               {`${stateValues.userInfo.data.user.title
                 ? `${getTitleValue(stateValues.userInfo.data.user.title, language)}`
                 : ''} ${stateValues.userInfo.data.user.firstName} ${stateValues.userInfo.data.user.lastName}`}
             </p>
-            <p className="user-info__item">
+            <p className={styles.item}>
               <b>
                 { t('userInfo.gender', {}) }
                 {' '}
               </b>
               {getGenderFieldValue(stateValues.userInfo.data.user.gender, language)}
             </p>
-            <p className="user-info__item">
+            <p className={styles.item}>
               <b>
                 { t('userInfo.dateOfBirth', {}) }
                 {' '}
@@ -79,7 +78,7 @@ const UserInfo = () => {
                 ? getDateValue(stateValues.userInfo.data.user.dateOfBirth)
                 : '' }
             </p>
-            <p className="user-info__item">
+            <p className={styles.item}>
               <b>
                 { t('userInfo.registrationDate', {}) }
                 {' '}
@@ -88,31 +87,31 @@ const UserInfo = () => {
                 ? getDateValue(stateValues.userInfo.data.user.registerDate)
                 : '' }
             </p>
-            <p className="user-info__item">
+            <p className={styles.item}>
               <b>
                 { t('userInfo.email', {}) }
                 {' '}
               </b>
               {stateValues.userInfo.data.user.email}
             </p>
-            <p className="user-info__item user-info__item_tel">
+            <p className={styles.item}>
               <b>
                 { t('userInfo.phone', {}) }
                 {' '}
               </b>
               {stateValues.userInfo.data.user.phone}
             </p>
-            <p className="user-info__item user-info__item_id">
+            <p className={styles.itemId}>
               <b>ID: </b>
               {stateValues.userInfo.data.user.id}
             </p>
           </div>
           { stateValues.login.data.authorizedUserId === params.id
             ? (
-              <button type="button" className="user-info__edit">
+              <button type="button" className={styles.edit}>
                 <UserInfoEditIcon />
                 <span
-                  className="user-info__edit-text"
+                  className={styles.editText}
                   onClick={() => dispatch(openUserModalAction())}
                 >
                   { t('editButton', {}) }
@@ -125,12 +124,12 @@ const UserInfo = () => {
     );
 
   return (
-    <div className={`${themeContext.darkTheme
-      ? 'user-info user-info_dark'
-      : 'user-info'}`}
+    <div className={themeContext.darkTheme
+      ? `${styles.info} ${styles.info_dark}`
+      : styles.info}
     >
       { stateValues.userInfo.data.error ? (
-        <div className="posts-list__error">
+        <div>
           { t('errorText', {}) }
         </div>
       ) : element }
--- examination_december_1/client/src/components/userComponents/userInfoEditIcon/UserInfoEditIcon.tsx
+++ examination_december_1/client/src/components/userComponents/userInfoEditIcon/UserInfoEditIcon.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import './UserInfoEditIcon.scss';
+import React, { useContext } from 'react';
+import styles from './UserInfoEditIcon.module.scss';
+import { ThemeContext } from '../../../contexts/ThemeContext';
 
-const UserInfoEditIcon = () => (
-  <svg
-    className="user-info__edit-svg"
-    viewBox="0 0 91 91"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <g>
-      <path d="M36.682,45.397l-0.002,0.001c-0.002,0.002-0.002,
+const UserInfoEditIcon = () => {
+  const themeContext = useContext(ThemeContext);
+
+  return (
+    <svg
+      className={themeContext.darkTheme
+        ? `${styles.svg} ${styles.svg_dark}`
+        : styles.svg}
+      viewBox="0 0 91 91"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g>
+        <path d="M36.682,45.397l-0.002,0.001c-0.002,0.002-0.002,
             0.004-0.002,0.004c-0.137,0.139-0.24,0.307-0.324,0.486
             c-0.023,0.045-0.039,0.086-0.057,0.133c-0.018,0.046-0.043,
             0.088-0.057,0.137l-4.008,14.826c-0.158,0.588,0.01,1.215,0.439,1.646
@@ -22,21 +28,22 @@ const UserInfoEditIcon = () => (
             1.25l-8.564-8.563L62.174,24.715z M58.518,28.372l8.564,8.563
             l-18.23,18.231l-8.566-8.564L58.518,28.372z M39.285,50.407l6.137,
             6.138l-9.135,2.471l2.469-9.136L39.285,50.407z"
-      />
-      <rect
-        height="15.087"
-        transform="matrix(0.7072 0.707 -0.707 0.7072 45.0547 -26.2009)"
-        width="3.4"
-        x="52.459"
-        y="33.749"
-      />
-      <path d="M20.961,74.166h45.74c3.596,0,6.518-2.925,
+        />
+        <rect
+          height="15.087"
+          transform="matrix(0.7072 0.707 -0.707 0.7072 45.0547 -26.2009)"
+          width="3.4"
+          x="52.459"
+          y="33.749"
+        />
+        <path d="M20.961,74.166h45.74c3.596,0,6.518-2.925,
             6.518-6.52V44.628H69.82v23.019c0,1.721-1.398,3.119-3.119
             ,3.119H22.662V25.662 h29.012v-3.4H20.961c-0.939,0-1.699,
             0.761-1.699,1.7v48.503C19.262,73.404,20.021,74.166,20.961,74.166z"
-      />
-    </g>
-  </svg>
-);
+        />
+      </g>
+    </svg>
+  );
+};
 
 export default UserInfoEditIcon;
