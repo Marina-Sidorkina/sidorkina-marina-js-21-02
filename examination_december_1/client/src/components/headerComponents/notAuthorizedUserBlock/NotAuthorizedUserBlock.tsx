1c34ccd538782489fada57c492e1d76727e9627f
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-12-13 02:34:03 +0300

    Refactor NotAuthorizedUserBlock styles

--- examination_december_1/client/src/components/headerComponents/notAuthorizedUserBlock/NotAuthorizedUserBlock.tsx
+++ examination_december_1/client/src/components/headerComponents/notAuthorizedUserBlock/NotAuthorizedUserBlock.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import './NotAuthorizedUserBlock.scss';
 import { Link } from 'react-router-dom';
 import '../../../locale/i18next';
 import { useTranslation } from 'react-i18next';
+import styles from './NotAuthorizedUserBlock.module.scss';
 
 const NotAuthorizedUserBlock = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="default-user-block">
-      <div className="default-user-block__item">
-        <Link className="default-user-block__link" to="/login">
-          <span className="default-user-block__signIn">
+    <div className={styles.userBlock}>
+      <div className={styles.item}>
+        <Link className={styles.link} to="/login">
+          <span className={styles.signIn}>
             { t('authorization.signIn', {}) }
           </span>
         </Link>
       </div>
-      <div className="default-user-block__item">
-        <Link className="default-user-block__link" to="/registration">
-          <span className="default-user-block__register">
+      <div className={styles.item}>
+        <Link className={styles.link} to="/registration">
+          <span className={styles.register}>
             { t('authorization.signUp', {}) }
           </span>
         </Link>
