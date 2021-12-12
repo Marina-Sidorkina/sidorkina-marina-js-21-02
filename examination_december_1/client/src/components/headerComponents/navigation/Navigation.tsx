55a80e6a9581e8cc2f6cf616b2bce0b76c5cfcb7
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-12-13 02:23:24 +0300

    Refactor navigation styles

--- examination_december_1/client/src/components/headerComponents/navigation/Navigation.tsx
+++ examination_december_1/client/src/components/headerComponents/navigation/Navigation.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
-import './Navigation.scss';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import styles from './Navigation.module.scss';
 import { ThemeContext } from '../../../contexts/ThemeContext';
 import NavigationPostsIcon from '../navigationPostsIcon/NavigationPostsIcon';
 import NavigationUsersIcon from '../navigationUsersIcon/NavigationUsersIcon';
@@ -12,29 +12,29 @@ const Navigation = () => {
   const { t } = useTranslation();
 
   return (
-    <nav className="navigation">
-      <ul className="navigation__list">
-        <li className="navigation__item navigation__item_users">
-          <Link className="navigation__link" to="/users">
+    <nav className={styles.navigation}>
+      <ul className={styles.list}>
+        <li className={styles.item}>
+          <Link className={styles.link} to="/users">
             <NavigationUsersIcon
               dark={themeContext.darkTheme}
             />
-            <span className={`${themeContext.darkTheme
-              ? 'navigation__text navigation__text_dark'
-              : 'navigation__text'}`}
+            <span className={themeContext.darkTheme
+              ? `${styles.text} ${styles.text_dark}`
+              : styles.text}
             >
               { t('navigation.users', {}) }
             </span>
           </Link>
         </li>
-        <li className="navigation__item navigation__item_posts">
-          <Link className="navigation__link" to="/posts">
+        <li className={styles.item}>
+          <Link className={styles.link} to="/posts">
             <NavigationPostsIcon
               dark={themeContext.darkTheme}
             />
-            <span className={`${themeContext.darkTheme
-              ? 'navigation__text navigation__text_dark'
-              : 'navigation__text'}`}
+            <span className={themeContext.darkTheme
+              ? `${styles.text} ${styles.text_dark}`
+              : styles.text}
             >
               { t('navigation.posts') }
             </span>
