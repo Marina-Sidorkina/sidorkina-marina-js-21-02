3c666173affc6b904d8a83f8164cb8c2631ee198
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-12-12 14:28:01 +0300

    Add config

---
+++ examination_december_1/client/src/api/proxy/config.ts
@@ -0,0 +1,10 @@
+export const PROXY_API_CONFIG = {
+  baseUrl: 'http://127.0.0.1:5000/proxy/',
+  statusOk: 200,
+  paths: {
+    user: 'user',
+    post: 'post',
+    comment: 'comment',
+    create: 'create'
+  }
+};
--- examination_december_1/client/src/api/proxy/index.tsx
+++ examination_december_1/client/src/api/proxy/index.tsx
@@ -1,51 +1,58 @@
 import axios, { AxiosResponse } from 'axios';
 import { INewUserData } from './@types/proxy';
+import { PROXY_API_CONFIG } from './config';
 
 const processResponse = (response: AxiosResponse<any, any>) => {
-  if (response.status !== 200) throw new Error('Could not fetch');
+  if (response.status !== PROXY_API_CONFIG.statusOk) throw new Error('Could not fetch');
   return response;
 };
 
-const proxy = axios.create({ baseURL: 'http://127.0.0.1:5000/proxy/' });
+const proxy = axios.create({ baseURL: PROXY_API_CONFIG.baseUrl });
 
-export const getPostInfo = (id: string) => proxy.get(`post/${id}`)
+export const getPostInfo = (id: string) => proxy.get(`${PROXY_API_CONFIG.paths.post}/${id}`)
   .then(processResponse);
 
-export const getUserInfo = (id: string) => proxy.get(`user/${id}`)
+export const getUserInfo = (id: string) => proxy.get(`${PROXY_API_CONFIG.paths.user}/${id}`)
   .then(processResponse);
 
 export const getPostsList = (
   page: number,
   limit: number,
-) => proxy.get('post', { params: { page, limit } })
+) => proxy.get(PROXY_API_CONFIG.paths.post, { params: { page, limit } })
   .then(processResponse);
 
 export const getUsersList = (
   page: number,
   limit: number
-) => proxy.get('user', { params: { page, limit } })
+) => proxy.get(PROXY_API_CONFIG.paths.user, { params: { page, limit } })
   .then(processResponse);
 
 export const getUserPosts = (
   page: number,
   limit: number,
   id: string
-) => proxy.get(`user/${id}/post`, { params: { page, limit } })
+) => proxy.get(`${PROXY_API_CONFIG.paths.user}/${id}/${PROXY_API_CONFIG.paths.post}`, { params: { page, limit } })
   .then(processResponse);
 
 export const getCommentsList = (
   page: number,
   limit: number,
   id: string
-) => proxy.get(`post/${id}/comment`, { params: { page, limit } })
+) => proxy.get(
+  `${PROXY_API_CONFIG.paths.post}/${id}/${PROXY_API_CONFIG.paths.comment}`,
+  { params: { page, limit } }
+)
   .then(processResponse);
 
 export const updateUser = (
   newUserData: Object,
   id: string
-) => proxy.put(`/user/${id}`, newUserData)
+) => proxy.put(`/${PROXY_API_CONFIG.paths.user}/${id}`, newUserData)
   .then(processResponse);
 
 export const addAndShowNewUser = (
   newUserData: INewUserData
-) => proxy.post('user/create', newUserData);
+) => proxy.post(
+  `${PROXY_API_CONFIG.paths.user}/${PROXY_API_CONFIG.paths.create}`,
+  newUserData
+);
